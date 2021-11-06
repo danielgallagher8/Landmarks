13399ece25ad534d759a10815a0a7a958b2cbce4
danielgallagher8 <daniel-gallagher@outlook.com>
2021-11-06 09:25:15 +0000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Daniel Gallagher on 06/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
